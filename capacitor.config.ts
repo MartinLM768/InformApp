b77b86783bdce81928bea17bc29db8b2f498818e
MartinLopez <martinlopez@ucundinamarca.edu.co>
2026-04-14 10:33:03 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'InforApp',
+  webDir: 'www'
+};
+
+export default config;
